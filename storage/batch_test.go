1fa18ce935f59da153637e1696b0fc46547500e4
brownJay <baowenjie20@163.com>
2023-10-31 13:41:02 +0800

    完善batchWrite test

--- storage/batch_test.go
+++ storage/batch_test.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"github.com/stretchr/testify/assert"
 	"kv-db-lab/model"
+	"strconv"
 	"testing"
 )
 
@@ -22,4 +23,25 @@ func TestWriteBatch(t *testing.T) {
 	key, err := db.Get([]byte("k11"))
 	assert.NotNil(t, err)
 	assert.Nil(t, key)
+}
+
+func TestWriteBatch_Commit(t *testing.T) {
+	db, err := OpenWithOptions(model.DefaultOptions)
+	assert.Nil(t, err)
+
+	wb := db.NewWriteBatch(model.DefaultWriteBatchOptions)
+	for i := 0; i < 1000; i++ {
+		err = wb.Put([]byte("2344"+strconv.Itoa(i)), []byte(strconv.Itoa(i)))
+		assert.Nil(t, err)
+	}
+	err = wb.Commit()
+	assert.Nil(t, err)
+
+	// 超出阈值的commit
+	for i := 0; i < 10000; i++ {
+		err = wb.Put([]byte("2344"+strconv.Itoa(i)), []byte(strconv.Itoa(i)))
+		assert.Nil(t, err)
+	}
+	err = wb.Commit()
+	assert.Nil(t, err)
 }
